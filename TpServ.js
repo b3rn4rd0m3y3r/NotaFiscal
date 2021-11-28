a7c2dbdd3a4a8975ba89ffd012137575baf1becc
b3rn4rd0m3y3r <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2021-11-28 11:47:18 -0300

    Add files via upload

---
+++ TpServ.js
@@ -0,0 +1,16 @@
+var TIPO_SERV = [
+		["No","Descri","Resumo","Quant"," VrUnit ","VrTotal","Item"],
+		[1,"Clientes residenciais e não residenciais REGIÃO METROPOLITANA E INTERIOR - telefone fixo e móvel até 15 minutos","Clientes resid/não resid RMBH/Interior tel fixo/móvel até 15 min",7.680," R$ 57,60 "," R$442.368,00 ","PESQUISA TELEFÔNICA"],
+		[2,"Clientes residenciais e não residenciais REGIÃO METROPOLITANA E INTERIOR - telefone fixo e móvel até 30 minutos","Clientes resid/não resid RMBH/Interior tel fixo/móvel até 30 min",7.680," R$ 72,00 "," R$552.960,00 ","PESQUISA TELEFÔNICA"],
+		[3,"Clientes residenciais e não residenciais REGIÃO METROPOLITANA E INTERIOR - telefone fixo e móvel até 50 minutos","Clientes resid/não resid RMBH/Interior tel fixo/móvel até 50 min",7.680," R$ 76,80 "," R$589.824,00 ","PESQUISA TELEFÔNICA"],
+		[4,"Clientes residenciais e não residenciais - REGIÃO METROPOLITANA DE BH – até 20 minutos","Clientes resid/não resid RMBH até 20 min",1.500," R$ 67,20 "," R$100.800,00 ","PESQUISA PRESENCIAL"],
+		[5,"Clientes residenciais e não residenciais - REGIÃO METROPOLITANA DE BH – até 40 minutos","Clientes resid/não resid RMBH até 40 min",1.500," R$ 84,00 "," R$126.000,00 ","PESQUISA PRESENCIAL"],
+		[6,"Clientes residenciais e não residenciais - CIDADES DO INTERIOR DE MG - até 20 minutos","Clientes resid/não resid INTERIOR até 20 min",1.500," R$ 76,80 "," R$115.200,00 ","PESQUISA PRESENCIAL"],
+		[7,"Clientes residenciais e não residenciais - CIDADES DO INTERIOR DE MG - até 40 minuto","Clientes resid/não resid INTERIOR até 40 min",1.500," R$ 86,40 "," R$129.600,00 ","PESQUISA PRESENCIAL"],
+		[8,"Clientes residenciais e não residenciais - REGIÃO METROPOLITANA DE BH","Clientes resid/não resid RMBH",36," R$ 13.900,80 "," R$500.428,80 ","FOCUS GROUP"],
+		[9,"Clientes residenciais e não residenciais - CIDADES DO INTERIOR DE MG","Clientes resid/não resid INTERIOR",24," R$ 12.000,00 "," R$288.000,00 ","FOCUS GROUP"],
+		[10,"Clientes residenciais e não residenciais, e poder concedente (PREFEITURAS)","Clientes resid/não resid e PREFEITURAS",140," R$ 1.440,00 "," R$201.600,00 ","ENTREVISTA EM PROFUNDIDADE"],
+		[11,"Analista técnico (em horas disponibilizada)","Analista técnico (em horas disponibilizada)",1.800," R$ 105,60 "," R$190.080,00 ","ANALISTA TÉCNICO"],
+		[12,"Desenvolvimento de sistema para visualização online dos dados produzidos mensal por Unidade de Negócios (Dashboard Mensal)","Desenv. de sistema p/visualização online dos dados mensal p/Unid. de Negócios (Dashboard Mensal)",36," R$ 2.880,00 "," R$103.680,00 ","RELATÓRIO DE ACOMPANHAMENTO MENSAL"],
+		[13,"Pesquisas respondidas","Pesquisas respondidas",300.000," R$ 0,96 "," R$288.000,00 ","PESQUISAS DE SATISFAÇÃO DE ATENDIMENTO - NPS (NET PROMOTER SCORE)"]
+		];

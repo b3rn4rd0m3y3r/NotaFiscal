0296bc9d1f46b6c0349254b07aef13d78b826705
b3rn4rd0m3y3r <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2021-12-20 18:27:55 -0300

    Add files via upload

---
+++ CRUD_selePedidoItem.php
@@ -0,0 +1,22 @@
+<?php
+	//$NoOS = $_GET["NoOs"];
+	$TpServ = $_GET["TpServ"];
+
+	include "connection.php";
+	// Teste de existência
+	// Gravamos os TpItem com "n-descrição"
+	// Devido aos acentos, testaremos apenas os dois primeiros caracteres
+	$sql = "SELECT sum(Quantidade) as Qtd from PedidoItens where TpItem Like ? ";
+	$params = array(
+		urldecode($TpServ) . "%"
+		);
+	echo urldecode($TpServ);
+	$sth = $conn->prepare($sql);
+	$sth->execute($params);
+	$Soma = 0;
+	if( $row = $sth->fetch() ){
+		echo  "[{\"Id\":\"EXISTE\"}, {\"Qtd\":\"" . $row['Qtd'] . "\"}]";
+		} else {
+		echo  "[{\"Id\":\"NAO EXISTE\"} , {\"Qtd\":\"0\"}]";
+		}
+?>
---
+++ TpServ_New.js
@@ -0,0 +1,35 @@
+// Constrói um select com os tipos de serviços do Array TIPO_SERV
+function html_tpServ(){
+	mostrAviso("Carregando tipos de serviços.");
+	var conteudo = "";
+	var index = "";
+	var max = TIPO_SERV.length;
+	var S = "";
+	
+	for(i=1;i<max;i++){
+		index = TIPO_SERV[i][0];
+		index = ("000"+(index).toString()).substr(-2);
+		conteudo = TIPO_SERV[i][2];
+		S += "<option value=\"" + index + "\">" + conteudo + "</option>";
+		console.log(index+" : "+conteudo);
+		}
+	//S = "<select >" + S + "</select>";
+	return S;
+	}
+var TIPO_SERV = [
+		["No","Descri","Resumo","Quant"," VrUnit ","VrTotal","Item"],
+		[1,"[Digitar a sua escolha]","[Escolha o Tipo]",0,"","","N/A"],
+		[2,"Clientes residenciais e não residenciais REGIÃO METROPOLITANA E INTERIOR - telefone fixo e móvel até 15 minutos","Clientes resid-não resid RMBH-Interior tel fixo/móvel até 15 min",7.680," R$ 57,60 "," R$442.368,00 ","PESQUISA TELEFÔNICA"],
+		[3,"Clientes residenciais e não residenciais REGIÃO METROPOLITANA E INTERIOR - telefone fixo e móvel até 30 minutos","Clientes resid-não resid RMBH-Interior tel fixo/móvel até 30 min",7.680," R$ 72,00 "," R$552.960,00 ","PESQUISA TELEFÔNICA"],
+		[4,"Clientes residenciais e não residenciais REGIÃO METROPOLITANA E INTERIOR - telefone fixo e móvel até 50 minutos","Clientes resid-não resid RMBH-Interior tel fixo/móvel até 50 min",7.680," R$ 76,80 "," R$589.824,00 ","PESQUISA TELEFÔNICA"],
+		[5,"Clientes residenciais e não residenciais - REGIÃO METROPOLITANA DE BH – até 20 minutos","Clientes resid-não resid RMBH até 20 min",1.500," R$ 67,20 "," R$100.800,00 ","PESQUISA PRESENCIAL"],
+		[6,"Clientes residenciais e não residenciais - REGIÃO METROPOLITANA DE BH – até 40 minutos","Clientes resid-não resid RMBH até 40 min",1.500," R$ 84,00 "," R$126.000,00 ","PESQUISA PRESENCIAL"],
+		[7,"Clientes residenciais e não residenciais - CIDADES DO INTERIOR DE MG - até 20 minutos","Clientes resid-não resid INTERIOR até 20 min",1.500," R$ 76,80 "," R$115.200,00 ","PESQUISA PRESENCIAL"],
+		[8,"Clientes residenciais e não residenciais - CIDADES DO INTERIOR DE MG - até 40 minuto","Clientes resid-não resid INTERIOR até 40 min",1.500," R$ 86,40 "," R$129.600,00 ","PESQUISA PRESENCIAL"],
+		[9,"Clientes residenciais e não residenciais - REGIÃO METROPOLITANA DE BH","Clientes resid-não resid RMBH",36," R$ 13.900,80 "," R$500.428,80 ","FOCUS GROUP"],
+		[10,"Clientes residenciais e não residenciais - CIDADES DO INTERIOR DE MG","Clientes resid-não resid INTERIOR",24," R$ 12.000,00 "," R$288.000,00 ","FOCUS GROUP"],
+		[11,"Clientes residenciais e não residenciais, e poder concedente (PREFEITURAS)","Clientes resid-não resid e PREFEITURAS",140," R$ 1.440,00 "," R$201.600,00 ","ENTREVISTA EM PROFUNDIDADE"],
+		[12,"Analista técnico (em horas disponibilizada)","Analista técnico (em horas disponibilizada)",1.800," R$ 105,60 "," R$190.080,00 ","ANALISTA TÉCNICO"],
+		[13,"Desenvolvimento de sistema para visualização online dos dados produzidos mensal por Unidade de Negócios (Dashboard Mensal)","Desenv. de sistema p-visualização online dos dados mensal p/Unid. de Negócios (Dashboard Mensal)",36," R$ 2.880,00 "," R$103.680,00 ","RELATÓRIO DE ACOMPANHAMENTO MENSAL"],
+		[14,"Pesquisas respondidas","Pesquisas respondidas",300.000," R$ 0,96 "," R$288.000,00 ","PESQUISAS DE SATISFAÇÃO DE ATENDIMENTO - NPS (NET PROMOTER SCORE)"]
+		];

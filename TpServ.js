f58f19e928cbdcb751478979a8f6c5e22425195c
b3rn4rd0m3y3r <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2022-01-10 22:35:19 -0300

    Add files via upload

--- TpServ.js
+++ TpServ.js
@@ -1,16 +1,13 @@
 var TIPO_SERV = [
 		["No","Descri","Resumo","Quant"," VrUnit ","VrTotal","Item"],
-		[1,"Clientes residenciais e não residenciais REGIÃO METROPOLITANA E INTERIOR - telefone fixo e móvel até 15 minutos","Clientes resid/não resid RMBH/Interior tel fixo/móvel até 15 min",7.680," R$ 57,60 "," R$442.368,00 ","PESQUISA TELEFÔNICA"],
-		[2,"Clientes residenciais e não residenciais REGIÃO METROPOLITANA E INTERIOR - telefone fixo e móvel até 30 minutos","Clientes resid/não resid RMBH/Interior tel fixo/móvel até 30 min",7.680," R$ 72,00 "," R$552.960,00 ","PESQUISA TELEFÔNICA"],
-		[3,"Clientes residenciais e não residenciais REGIÃO METROPOLITANA E INTERIOR - telefone fixo e móvel até 50 minutos","Clientes resid/não resid RMBH/Interior tel fixo/móvel até 50 min",7.680," R$ 76,80 "," R$589.824,00 ","PESQUISA TELEFÔNICA"],
-		[4,"Clientes residenciais e não residenciais - REGIÃO METROPOLITANA DE BH – até 20 minutos","Clientes resid/não resid RMBH até 20 min",1.500," R$ 67,20 "," R$100.800,00 ","PESQUISA PRESENCIAL"],
-		[5,"Clientes residenciais e não residenciais - REGIÃO METROPOLITANA DE BH – até 40 minutos","Clientes resid/não resid RMBH até 40 min",1.500," R$ 84,00 "," R$126.000,00 ","PESQUISA PRESENCIAL"],
-		[6,"Clientes residenciais e não residenciais - CIDADES DO INTERIOR DE MG - até 20 minutos","Clientes resid/não resid INTERIOR até 20 min",1.500," R$ 76,80 "," R$115.200,00 ","PESQUISA PRESENCIAL"],
-		[7,"Clientes residenciais e não residenciais - CIDADES DO INTERIOR DE MG - até 40 minuto","Clientes resid/não resid INTERIOR até 40 min",1.500," R$ 86,40 "," R$129.600,00 ","PESQUISA PRESENCIAL"],
-		[8,"Clientes residenciais e não residenciais - REGIÃO METROPOLITANA DE BH","Clientes resid/não resid RMBH",36," R$ 13.900,80 "," R$500.428,80 ","FOCUS GROUP"],
-		[9,"Clientes residenciais e não residenciais - CIDADES DO INTERIOR DE MG","Clientes resid/não resid INTERIOR",24," R$ 12.000,00 "," R$288.000,00 ","FOCUS GROUP"],
-		[10,"Clientes residenciais e não residenciais, e poder concedente (PREFEITURAS)","Clientes resid/não resid e PREFEITURAS",140," R$ 1.440,00 "," R$201.600,00 ","ENTREVISTA EM PROFUNDIDADE"],
-		[11,"Analista técnico (em horas disponibilizada)","Analista técnico (em horas disponibilizada)",1.800," R$ 105,60 "," R$190.080,00 ","ANALISTA TÉCNICO"],
-		[12,"Desenvolvimento de sistema para visualização online dos dados produzidos mensal por Unidade de Negócios (Dashboard Mensal)","Desenv. de sistema p/visualização online dos dados mensal p/Unid. de Negócios (Dashboard Mensal)",36," R$ 2.880,00 "," R$103.680,00 ","RELATÓRIO DE ACOMPANHAMENTO MENSAL"],
-		[13,"Pesquisas respondidas","Pesquisas respondidas",300.000," R$ 0,96 "," R$288.000,00 ","PESQUISAS DE SATISFAÇÃO DE ATENDIMENTO - NPS (NET PROMOTER SCORE)"]
+		[1,"Saco de batatas de 50 Kg","Batata",7.680," R$ 57,60 "," R$442.368,00 ","Saco"],
+		[2,"Caixa de Bananas 50 Kg","Banana",7.680," R$ 72,00 "," R$552.960,00 ","Caixa"],
+		[3,"Caixa de quiabo 50 Kg","Quiabo",7.680," R$ 76,80 "," R$589.824,00 ","Caixa"],
+		[4,"Caixa de tomates 50 Kg resfriada","Tomates",1.500," R$ 67,20 "," R$100.800,00 ","Caixa"],
+		[5,"Saco de Laranjas 50 Kg resfriada","Laranjas",1.500," R$ 84,00 "," R$126.000,00 ","Saco"],
+		[6,"Caixa de Caqui chocolate 5 Kg","Caqui",1.500," R$ 76,80 "," R$115.200,00 ","Caixa"],
+		[7,"Caixa com 12 garrafas de whisky","Whisky",1.500," R$ 86,40 "," R$129.600,00 ","Caixa"],
+		[8,"Caixa com 6 garrafas de um litro de suco de Uva","Suco de Uva",36," R$ 13.900,80 "," R$500.428,80 ","Caixa"],
+		[9,"Caixa com 8 garrafas de um litro de suco de laranja","Suco de Laranja",24," R$ 12.000,00 "," R$288.000,00 ","Caixa"],
+		[10,"Caixa com 24 caixinhas de Leite Condensado","Leite Condensado",140," R$ 1.440,00 "," R$201.600,00 ","Caixa"]
 		];
